4f399847555007a88504612644d7e3bb5e36741a
Briyan A <briyana1997@gmail.com>
2023-02-13 20:29:15 -0400

    gh pages add

--- src/services/product/getProduct.ts
+++ src/services/product/getProduct.ts
@@ -1,7 +1,7 @@
 import {Product} from "../../../types"
 
 const getAllProducts = ():Promise<Product[]> => {
-    return fetch("http://192.168.0.9:3001/product",{method:"GET"})
+    return fetch("http://localhost:3001/product",{method:"GET"})
     .then(response => response.json())
 }
 
